a1685d0211b6d5773b23c0fd1938cf1c5f09b78c
Anna Natorilla <annagaiel@gmail.com>
2016-06-06 20:39:08 -0700

    Add Facility class

--- lib/sf_mobile_food_facilities.rb
+++ lib/sf_mobile_food_facilities.rb
@@ -1,5 +1,26 @@
 require "sf_mobile_food_facilities/version"
+require 'unirest'
 
 module SfMobileFoodFacilities
-  # Your code goes here...
+  class Facility
+
+    attr_reader :address, :applicant, :dayshours, :facilitytype, :fooditems
+
+    def initialize(hash)
+      @address = hash["address"]
+      @applicant = hash["applicant"]
+      @dayshours = hash["dayshours"]
+      @facilitytype = hash["facilitytype"]
+      @fooditems = hash["fooditems"]
+    end
+
+    def self.all
+      facilities_array = Unirest.get("https://data.sfgov.org/resource/6a9r-agq8.json").body
+      facilities = []
+      facilities_array.each do |facility_hash|
+          facilities << Facility.new(facility_hash)
+      end
+      return facilities
+    end
+  end
 end
